c62326c89605ffb1cbe16deb8d96dc72b6da2b73
SRamakrishna <ramakrishnanaidu688@gmail.com>
2022-07-13 17:08:00 +0530

    basic

---
+++ data_type.py
@@ -0,0 +1,15 @@
+x=5
+y=5.5
+z="siva"
+list=["siva",'rama','krishna']
+tuple=('c','lsp','python')
+dict={'key':5}
+set={'hii','how','are','you'}
+
+print(x)
+print(y)
+print(z)
+print(list)
+print(tuple)
+print(dict)
+print(set)
---
+++ type_casting.py
@@ -0,0 +1,13 @@
+x=int(1)
+y=int(2.0)
+z=int('3')
+
+a=float(1)
+b=float(2.0)
+c=float('3')
+
+m=str(1)
+n=str(2.0)
+o=str('3.0')
+
+print(x,y,z,a,b,c,m,n,o)
